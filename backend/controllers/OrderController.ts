8461214dee4726c3bb6cab3f5a1a3ed3dce936f8
elsabilah1 <elsabilah1@gmail.com>
2022-01-03 00:04:13 +0700

    fix

--- backend/controllers/OrderController.ts
+++ backend/controllers/OrderController.ts
@@ -4,7 +4,6 @@ import Customer from '@backend/models/Customer'
 
 const OrderController = {
   create: async function (req: Request, res: Response, next: NextFunction) {
-<<<<<<< HEAD
     const { authorization } = req.headers
 
     if (authorization) {
@@ -79,26 +78,6 @@ const OrderController = {
               return true
             }
           })
-=======
-    const { authorization } = req.headers;
-    const token = authorization.split(" ")[1];
-    const { data } = req.body;
-
-    const r = verifyToken(String(token));
-    let example;
-
-    if (typeof r.user !== "undefined") {
-      const dataCustomer = await Customer.findById(r.user._id);
-      if (dataCustomer.order.length === 0) {
-        dataCustomer.order.push(data);
-      } else {
-        const test = dataCustomer.order.filter((res) => {
-          return res.order_id === data.order_id;
-        });
-
-        if (test.length === 0) {
-          dataCustomer.order.push(data);
->>>>>>> 08c1a034aafa99b86fe2a568264103ab261daa05
         }
 
         orders = handleSkip(skip)
@@ -110,7 +89,6 @@ const OrderController = {
           result: orders.length,
         })
       }
-<<<<<<< HEAD
     }
   },
   getOrderDetails: async function (
@@ -259,19 +237,6 @@ const OrderController = {
           message: 'updated successfully',
         })
       }
-=======
-      await dataCustomer.save();
-      example = dataCustomer.order.filter(
-        (v, i, a) => a.findIndex((t) => t.order_id === v.order_id) === i
-      );
-      dataCustomer.order = example;
-      await dataCustomer.save();
-
-      return res.status(201).json({
-        status: 201,
-        example,
-      });
->>>>>>> 08c1a034aafa99b86fe2a568264103ab261daa05
     }
   },
 }
