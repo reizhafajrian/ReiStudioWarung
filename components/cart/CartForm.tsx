a12a1a2581f526f5894360025c433c03b8c68c92
reizha <reizha@importir.co>
2022-01-28 01:18:52 +0700

    fix midtrans

--- components/cart/CartForm.tsx
+++ components/cart/CartForm.tsx
@@ -1,145 +1,145 @@
-import * as React from 'react'
-import { RootStateOrAny, useDispatch, useSelector } from 'react-redux'
-import Script from 'next/script'
-import { CButton, CForm, CFormInput } from '@coreui/react'
-import { Get, Post } from 'utils/axios'
-import { deleteAllItem } from 'redux/actions/cartActions'
-import cookie from 'js-cookie'
-import { useRouter } from 'next/router'
+import * as React from "react";
+import { RootStateOrAny, useDispatch, useSelector } from "react-redux";
+import Script from "next/script";
+import { CButton, CForm, CFormInput } from "@coreui/react";
+import { Get, Post } from "utils/axios";
+import { deleteAllItem } from "redux/actions/cartActions";
+import cookie from "js-cookie";
+import { useRouter } from "next/router";
 
 declare global {
   interface Window {
-    snap: any
+    snap: any;
   }
 }
 
 const OrderDetails = ({ user }: any) => {
-  const token = cookie.get('token')
-  const dispatch = useDispatch()
-  const router = useRouter()
+  const token = cookie.get("token");
+  const dispatch = useDispatch();
+  const router = useRouter();
 
-  const { cart } = useSelector((state: RootStateOrAny) => state)
-  const [total, setTotal] = React.useState(cart.total)
-  const [voucher, setVoucher] = React.useState({})
-  const [code, setCode] = React.useState('')
-  const [isDiscount, setIsDiscount] = React.useState(false)
+  const { cart } = useSelector((state: RootStateOrAny) => state);
+  const [total, setTotal] = React.useState(cart.total);
+  const [voucher, setVoucher] = React.useState({});
+  const [code, setCode] = React.useState("");
+  const [isDiscount, setIsDiscount] = React.useState(false);
 
   const handleVoucher = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCode(e.target.value)
+    setCode(e.target.value);
     Get(`/vouchers?code=${e.target.value}`, token).then((res: any) => {
       if (res.voucher) {
-        const calculate = cart.total - res.voucher.amount
-        setTotal(calculate < 0 ? 0 : calculate)
-        setIsDiscount(true)
-        setVoucher(res.voucher)
+        const calculate = cart.total - res.voucher.amount;
+        setTotal(calculate < 0 ? 0 : calculate);
+        setIsDiscount(true);
+        setVoucher(res.voucher);
       } else {
-        setIsDiscount(false)
+        setIsDiscount(false);
       }
-    })
-  }
+    });
+  };
 
-  const fullname = user.name.split(' ')
+  const fullname = user.name.split(" ");
 
   const handlePost = async () => {
-    const post: any = await Post('/customer/pay', {
-      price: total,
+    const post: any = await Post("/customer/pay", {
+      price: cart.total,
       email: user.email,
       first_name: fullname[0],
       last_name: fullname[fullname.length - 1],
       phone: user.phone,
-    })
+    });
     // alert("s")
     // window.location.href = post.transaction.redirect_url;
 
     window.snap.pay(post.transaction.token, {
       onSuccess: async (result: any) => {
-        console.log(result, 'result')
+        console.log(result, "result");
       },
       onPending: async (result: any) => {
         Get(`/customer/pay?id=${result.order_id}`, token).then((res: any) => {
-          if (res.response.transaction_status === 'pending') {
-            Post('/customer/createorder', {
+          if (res.response.transaction_status === "pending") {
+            Post("/customer/createorder", {
               data: {
                 order_id: res.response.order_id,
                 cart: cart.cartItems,
                 voucher: voucher,
                 total: total,
                 status: {
-                  title: 'sedang diproses',
-                  content: '',
+                  title: "sedang diproses",
+                  content: "",
                 },
                 payment: res.response.store,
                 created_at: res.response.transaction_time,
               },
             }).then((res) => {
-              console.log(res)
+              console.log(res);
 
-              router.push('/customer/profile')
-              Post('/customer/addtocart', {
+              router.push("/customer/profile");
+              Post("/customer/addtocart", {
                 data: [],
-              })
-              dispatch(deleteAllItem())
-            })
+              });
+              dispatch(deleteAllItem());
+            });
           }
-        })
+        });
       },
       onError: async (result: any) => {
-        return alert('Error')
+        return alert("Error");
         // return settemp(temp.concat(result));
       },
-    })
-  }
+    });
+  };
 
   return (
     <>
       <Script
-        src='https://app.sandbox.midtrans.com/snap/snap.js'
-        data-client-key='SB-Mid-client-A5zLo_R0ygqCcWAO'
+        src="https://app.sandbox.midtrans.com/snap/snap.js"
+        data-client-key="SB-Mid-client-A5zLo_R0ygqCcWAO"
       />
       <CForm>
-        <div className='mb-3'>
-          <h5 className='fw-bold'>Alamat Pengiriman</h5>
+        <div className="mb-3">
+          <h5 className="fw-bold">Alamat Pengiriman</h5>
           <CFormInput
-            placeholder='Tambah alamat'
-            className='border-0 border-bottom border-2 rounded-0 ps-2 py-0'
+            placeholder="Tambah alamat"
+            className="border-0 border-bottom border-2 rounded-0 ps-2 py-0"
             readOnly
             value={user.address}
           />
         </div>
-        <div className='mb-3'>
-          <h5 className='fw-bold'>Voucher Belanja</h5>
+        <div className="mb-3">
+          <h5 className="fw-bold">Voucher Belanja</h5>
           <CFormInput
-            type='search'
-            className='border-0 border-bottom border-2 rounded-0 ps-2 py-0'
+            type="search"
+            className="border-0 border-bottom border-2 rounded-0 ps-2 py-0"
             onChange={(e) => handleVoucher(e)}
           />
-          {!isDiscount && code && <p className='text-danger'>invalid code</p>}
+          {!isDiscount && code && <p className="text-danger">invalid code</p>}
         </div>
-        <div className='mb-3'>
-          <h5 className='fw-bold'>Total harga</h5>
+        <div className="mb-3">
+          <h5 className="fw-bold">Total harga</h5>
           <h5
             className={`${
-              isDiscount && 'text-gray m-0 text-decoration-line-through'
+              isDiscount && "text-gray m-0 text-decoration-line-through"
             }`}
           >
             Rp.
-            {cart.total.toLocaleString('id-ID')}
+            {cart.total.toLocaleString("id-ID")}
             ,-
           </h5>
           {isDiscount && (
-            <h5 className='text-success m-0 mt-2'>
+            <h5 className="text-success m-0 mt-2">
               Rp.
-              {total.toLocaleString('id-ID')}
+              {total.toLocaleString("id-ID")}
               ,-
             </h5>
           )}
         </div>
-        <div className='text-center'>
+        <div className="text-center">
           <CButton
-            className='w-auto text-white px-3 py-2'
+            className="w-auto text-white px-3 py-2"
             onClick={(e) => {
-              e.preventDefault()
-              handlePost()
+              e.preventDefault();
+              handlePost();
             }}
           >
             Pesan Sekarang
@@ -147,7 +147,7 @@ const OrderDetails = ({ user }: any) => {
         </div>
       </CForm>
     </>
-  )
-}
+  );
+};
 
-export default React.memo(OrderDetails)
+export default React.memo(OrderDetails);
